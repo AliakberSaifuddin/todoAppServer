d9865940f9c933dc5c29a2806a80ce15b4092beb
Aliakber <aliakber.saif123@gmail.com>
2020-07-01 12:19:59 +0500

    same app as deployed on heroku but name different

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const mongoose = require('mongoose');
 
 
 const url = 'mongodb://localhost/TodoDB';
-const connect = mongoose.connect("mongodb+srv://aliakber:aliakber5253@cluster0.89wzb.mongodb.net/Todo" || url, {useNewUrlParser:true});
+const connect = mongoose.connect( process.env.MONGODB_URI || url, {useNewUrlParser:true, useUnifiedTopology: true});
 var cors = require('cors')
 
 connect.then((db) => {
@@ -55,11 +55,11 @@ app.use('/api/todos', todoRouter);
 
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
